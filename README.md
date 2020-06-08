77decc1849cf380dffe5d8430e81ee4b920c0208
Peter Kaminski <kaminski@istori.com>
2020-06-07 18:25:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Images by SMS
+
+## Development Status
+
+**INCOMPLETE**
+
+## Overview
+
+Receive inbound MMS images from Twilio, and distribute to Airtable,
+Google Drive, and Slack.
+
+**Images by SMS** is written in Python 3.
+
+## Setting Up The Airtable Bases
+
+(TBD)
+
+## Installation
+
+Clone or download the repository.
+
+```shell
+git clone https://github.com/peterkaminski/images-by-sms.git && cd images-by-sms
+```
+
+## Script Configuration
+
+Copy `env.sh-template` to `env.sh`, and then replace the dummy API key value with your own Airtable API key and base IDs.
+
+## Python Configuration
+
+To isolate the libraries and versions used by this script, create a venv, then install the libraries.
+
+```shell
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Run Airtable Schema to Base
+
+Load your API key, Airtable bases, and option local test parameters.
+
+```shell
+source env.sh
+```
+
+Run the `images-by-sms.py` script.
+
+```shell
+./images-by-sms.py
+```
+
+## Feedback, Suggestions, Bugs
+
+Feedback is welcome either as Issues or Pull Requests at the [Images by SMS repo](https://github.com/peterkaminski/images-by-sms).
+
+## License
+
+Copyright (c) 2020 Peter Kaminski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ images-by-sms.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+# PyPI
+from airtable import Airtable # pip install airtable-python-wrapper
+from flask import Flask, request # pip install flask
+from twilio.twiml.messaging_response import MessagingResponse # pip install twilio
+import pendulum # pip install pendulum
+
+# Python
+from base64 import b64decode, b64encode
+from random import SystemRandom
+import hashlib
+import hmac
+import os
+import re
+
+# get auth keys from environment
+airtable_api_key = os.environ['AIRTABLE_API_KEY']
+airtable_base_phoso = os.environ['AIRTABLE_BASE_PHOSO']
+airtable_base_images_by_sms = os.environ['AIRTABLE_BASE_IMAGES_BY_SMS']
+
+# set up Airtable connections
+messages_table = Airtable(airtable_base_phoso, 'Messages', api_key=airtable_api_key)
+photos_table = Airtable(airtable_base_phoso, 'Photos', api_key=airtable_api_key)
+senders_table = Airtable(airtable_base_phoso, 'Senders', api_key=airtable_api_key)
+chapters_table = Airtable(airtable_base_images_by_sms, 'Chapter Setup', api_key=airtable_api_key)
+
+def create_sender_id(recipient, sender):
+    hash = hmac.digest(recipient.encode(), sender.encode(), 'sha256')
+    hash = b64encode(hash).decode('utf-8').upper()
+    return re.sub(r'[^A-Z]', '', hash)[:8]
+
+def assemble_filename(data):
+    # get date received in UTC
+    date_received = pendulum.now()
+
+    # get chapter data
+    chapter_row = chapters_table.match('SMS Phone Number', data['To Phone'])
+    chapter_abbreviation = chapter_row['fields']['City Name Abbreviation']
+    chapter_timezone = chapter_row['fields']['Timezone']
+    date_received_local = date_received.in_timezone(chapter_timezone)
+
+    # get local date and time
+    yyyy_mm_dd = date_received_local.strftime('%Y-%m-%d')
+    hhmm = date_received_local.strftime('%H%M')
+
+    # get sender_id from to, from
+    sender_id = create_sender_id(data['To Phone'], data['From Phone'])
+
+    # get two random digits
+    random_digits = SystemRandom().randint(0,99)
+
+    # assemble filename
+    return '{}_{}_{}_{}_{}'.format(yyyy_mm_dd, sender_id, hhmm, random_digits, chapter_abbreviation)
+
+def post_to_airtable(data):
+    del data['Message']  # linked field, needs to be handled differently
+    del data['To Phone']  # not used
+    del data['From Phone']  # not used
+    photos_table.insert(data)
+    print("post_to_airtable:")
+    print(data)
+
+def post_to_gdrive(data):
+    print("post_to_gdrive:")
+    print(data)
+
+def post_to_slack(data):
+    print("post_to_slack:")
+    print(data)
+
+def handle_photo(data):
+    data['Filename'] = assemble_filename(data)
+    post_to_airtable(data)
+    post_to_gdrive(data)
+    post_to_slack(data)
+
+def main():
+    data = {
+        'Message': os.environ['MESSAGE_BODY'],
+        'To Phone': os.environ['TO_PHONE'],
+        'From Phone': os.environ['FROM_PHONE'],
+        'Photo': [{'url': os.environ['MEDIA_URL']}]
+    }
+    handle_photo(data)
+
+app = Flask(__name__)
+
+@app.route("/images-by-sms", methods=['GET', 'POST'])
+def webhook_images_by_sms():
+
+    try:
+        # Start our response
+        resp = MessagingResponse()
+
+        # set up data dict from form data
+        data = {
+            'Message': request.form['Body'],
+            'To Phone': request.form['To'],
+            'From Phone': request.form['From']
+        }
+
+        # add first photo if it exists
+        if 'MediaUrl0' in request.form:
+            data['Photo'] = [{'url': request.form['MediaUrl0']}]
+
+        # TODO: support multiple photos
+        handle_photo(data)
+
+        # respond
+        resp.message("Message received!")
+
+        # send response
+        return str(resp)
+    except Exception as e:
+        print(e)
+        return ''
+
+if __name__ == "__main__":
+    if os.environ['TO_PHONE']:
+        exit(main())
+    else:
+        app.run(port=80, debug=True)
